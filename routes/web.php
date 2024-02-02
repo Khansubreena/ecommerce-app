b2d39c6803127ba7d6b24ad4e09b3d7bc99a2ded
khansubreena <subreenakhan27@gmail.com>
2024-02-02 15:55:06 +0530

    auth login and logout completed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,28 @@ use App\Http\Controllers\admin\AdminLoginController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.login');
 });
 
-Route::get('/',[AdminLoginController::class,'index'])->name('admin.login');
+// Route::get('/',[AdminLoginController::class,'index'])->name('admin.login');//
 
+Route::group(['prefix' => 'admin'], function(){
+
+
+    Route::group(['middleware' => 'admin.guest'],function(){
+     
+
+        Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
+        Route::post('/authentictae',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+       
+
+    });
+
+    Route::group(['middleware' => 'admin.auth'],function(){
+        Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
+        Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+
+    });
+
+    
+});
